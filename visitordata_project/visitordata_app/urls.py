acbd0fdd3dac92e6874b7c283f5baa35bd743b8a
pullpanther <softechstar.wlc@outlook.com>
2023-07-12 20:26:29 +0200

    add option to delete session data

--- visitordata_project/visitordata_app/urls.py
+++ visitordata_project/visitordata_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.visitor, name="homepage"),
+    path("delete-session/", views.delete_session_item, name="delete_session")
 ]
--- visitordata_project/visitordata_app/views.py
+++ visitordata_project/visitordata_app/views.py
@@ -12,14 +12,34 @@ def visitor(request):
     ip = get_ip(request)
 
     geo_data = request.session.get("geolocation")
+    is_from_session = True
     if not geo_data:
         geo_data = get_geolocation_data(request)
+        is_from_session = False
         request.session["geolocation"] = geo_data
 
     context = {"meta": meta,
                "ua": ua,
                "ip": ip,
                "geo_data": geo_data,
+               "is_from_session": is_from_session,
                }
 
     return render(request, "visitordata_app/visitor.html", context)
+
+
+def delete_session_item(request):
+    # url parameters ?item=abc&next=xyz
+    item = request.GET.get("item", "")
+    next_page = request.GET.get("next", "")
+
+    try:
+        del request.session[item]
+    # if url item parameter does not exist in session, delete complete session
+    except KeyError:
+        request.session.flush()
+
+    if next_page:
+        return redirect(next_page)
+
+    return redirect("homepage")
